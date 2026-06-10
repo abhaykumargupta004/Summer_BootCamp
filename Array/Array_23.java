fbc3ec13a3f7476749f2c586ee1f24ef0848dfb5
abhaykumargupta004 <abhaykumargupta004@gmail.com>
2026-06-10 22:09:56 +0530

    Add files via upload

--- Array/Array_23.java
+++ Array/Array_23.java
@@ -1,32 +1,36 @@
 // Max Product SubArray
-import java.util.*;
-class Array_23{
-    static int Max_Product(int arr[]){
-        int product=1;
-        int start =0;
-        int end=0;
-        int maxProduct=1;
-        for(int i=0; i<arr.length; i++){
-            start =i;
-            product *= arr[i]; 
-            maxProduct = Math.max(maxProduct, product);
-            if(product<0){
-                end=i;
-                product=1;
 
+import java.util.*;
+
+class Array_23 {
+
+    static int Max_Product(int arr[]) {
+        int minproduct = arr[0];
+        int maxproduct = arr[0];
+
+        int ans = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                int temp = maxproduct;
+                maxproduct = minproduct;
+                minproduct = temp;
             }
+            maxproduct = Math.max(arr[i], maxproduct * arr[i]);
+            minproduct = Math.min(arr[i], minproduct * arr[i]);
+            ans = Math.max(ans, maxproduct);
 
         }
-        return maxProduct;
+        return ans;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
+        int n = sc.nextInt();
         int arr[] = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         int result = Max_Product(arr);
-        System.out.println("Maximum product is: " +result);
+        System.out.println("Maximum product is: " + result);
     }
 }
