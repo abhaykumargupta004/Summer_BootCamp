470ec23b1e7310f352f6dc2e648874abbfc444ce
AbhayKumarGupta01 <guptag7007313237@gmail.com>
2026-07-08 23:00:02 +0530

    .

---
+++ Array/ThreeSum.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+public class ThreeSum {
+
+    List<List<Integer>> threeSum(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    j++;
+                } else if (sum > target) {
+                    k--;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of Array: ");
+        int n = sc.nextInt();
+        int target = 0;
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        ThreeSum obj = new ThreeSum();
+        System.out.println(obj.threeSum(nums, target));
+    }
+}
