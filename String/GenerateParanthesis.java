a208bdaa0afc14d9d314e267fca14efc1311d3a7
Abhay Kumar Gupta <abhaykumargupta004@gmail.com>
2026-08-23 23:25:23 +0530

    Generate Paranthesis

---
+++ String/GenerateParanthesis.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class GenerateParenthesis {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+
+        backtrack(result, "", 0, 0, n);
+
+        return result;
+    }
+
+    public void backtrack(List<String> result, String current,
+                          int open, int close, int n) {
+
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+
+    public static void main(String[] args) {
+        GenerateParenthesis obj = new GenerateParenthesis();
+
+        System.out.println(obj.generateParenthesis(3));
+    }
+}
